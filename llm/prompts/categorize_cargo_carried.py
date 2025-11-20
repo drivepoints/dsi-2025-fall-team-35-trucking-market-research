a1d7599d1c14e6875c95d7b998368ddf626472d4
Jared Donohue <jared.donohue@gmail.com>
2025-11-20 18:59:10 -0500

    Add new ground_truth datasets and corresponding script; begin LLM prompt for re-categorizing cargo_carried

---
+++ llm/prompts/categorize_cargo_carried.py
@@ -0,0 +1,46 @@
+"""
+You are categorizing the "cargo carried" for U.S. trucking companies according to 
+The FMCSA Safety and Fitness Electronic Records (SAFER) System which defines the following categories for "cargo carried".
+
+Your job is to standardize the categorization of the cargo carried so that instead of having ~90,000 unique entries, there are less than 100.
+
+The following 29 categories are defined in the SAFER System:
+General Freight
+Household Goods
+Metal: sheets, coils, rolls
+Motor Vehicles
+Drive/Tow away
+Logs, Poles, Beams, Lumber
+Building Materials
+Mobile Homes
+Machinery, Large Objects
+Fresh Produce
+SAFER Layout
+Liquids/Gases
+Intermodal Cont.
+Passengers
+Oilfield Equipment
+Livestock
+Grain, Feed, Hay
+Coal/Coke
+Meat
+Garbage/Refuse
+US Mail
+SAFER Layout
+Chemicals
+Commodities Dry Bulk
+Refrigerated Food
+Beverages
+Paper Products
+Utilities
+Agricultural/Farm Supplies
+Construction
+Water Well
+
+
+5 other common categories have been discovered through manual inspection:
+Lawn Equipment
+Farm Use
+Sediments and Gravel
+Food
+"""
